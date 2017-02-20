2e2a8ac3c541a63af50cf7b0d0c33f435c2cd883
Dines Poulsen <dinespoulsen84@gmail.com>
2017-02-20 18:12:34 +0000

    Made menu responive

---
+++ index.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+
+  var menu = document.getElementById("responsive-menu");
+  var menuList = document.getElementById("menu");
+  menu.addEventListener("click", function(){
+    if(menu.getAttribute("toggle") != "on"){
+      menu.setAttribute("toggle", "on");
+      menuList.style.display = "inline-block";
+    }
+    else {
+      menu.setAttribute("toggle", "off");
+      menuList.style.display = "none";
+    }
+  });
+
+  window.addEventListener('resize', function(){
+    if(window.innerWidth >= 600 ) {
+      menuList.style.display = "inline-block";
+    }
+    else {
+      menuList.style.display = "none";
+    }
+  });
+};
